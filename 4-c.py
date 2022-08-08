7a63359c50edf1f3c08a63b8b03071c1513c4dc8
newyear2580 <51331397+newyear2580@users.noreply.github.com>
2022-08-08 13:12:54 +0800

    Create 4-c.py

---
+++ 4-c.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+
+X = np.arange (-5, 5, 0.5 )
+Y = np.arange (-5, 5, 0.5 )
+x,y = np.meshgrid ( X, Y )
+
+dy = x * y # 微分方程式 
+dx = 1
+norm = np.sqrt ( dx * dx + dy * dy ) # 正規化
+dy = dy / norm
+dx = dx / norm
+plt.figure(1)
+q = plt.quiver(x, y, dx, dy)# 方向場繪圖
+
+
+plt.xlabel ( 'x')
+plt.ylabel ( 'y')
+plt.title ( 'Plot of the Function : dy/dx = x * y' )
+plt.grid()
+plt.text(1.5,-4.8, 'Copyright@10627109' )
+plt.show()
